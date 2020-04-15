70b4d1004f523fc0418eb4428e91003f912893db
czj111 <1791902635@qq.com>
2020-04-15 23:14:10 +0800

    解决文件上传大小，临时文件，文件名重复问题

--- src/main/java/cn/itcast/travel/web/servlet/upLoadServlet.java
+++ src/main/java/cn/itcast/travel/web/servlet/upLoadServlet.java
@@ -5,6 +5,7 @@ import org.apache.commons.fileupload.FileUploadException;
 import org.apache.commons.fileupload.disk.DiskFileItemFactory;
 import org.apache.commons.fileupload.servlet.ServletFileUpload;
 
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.ServletInputStream;
 import javax.servlet.annotation.WebServlet;
@@ -19,14 +20,21 @@ import static java.lang.System.out;
 @WebServlet("/upLoadServlet")
 public class upLoadServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        //判断请求是否为Multipart
         if(!ServletFileUpload.isMultipartContent(request))
         {
             throw new RuntimeException("当前请求不支持文件上传");
         }
+        String path_temp=this.getServletContext().getRealPath("/temp");
+        File file_temp=new File(path_temp);
         // Create a factory for disk-based file items
-        DiskFileItemFactory factory = new DiskFileItemFactory();
+        DiskFileItemFactory factory = new DiskFileItemFactory(1024*1024,file_temp);
         // Create a new file upload handler
         ServletFileUpload upload = new ServletFileUpload(factory);
+        //解决文件名乱码问题。
+        upload.setHeaderEncoding("UTF-8");
+        //限制每个文件上传大小
+        upload.setFileSizeMax(1024*1024*100);
         // Parse the request
         List<FileItem> items=null;
         try {
@@ -40,12 +48,14 @@ public class upLoadServlet extends HttpServlet {
             if(item.isFormField())
             {
                 String fieldName=item.getFieldName();
-                String fieldValue=item.getString();
+                String fieldValue=item.getString("UTF-8");
                 out.println(fieldName+"="+fieldValue);
             }
             else
             {
                 String Name=item.getName();
+                //防止文件名重复
+                Name=System.currentTimeMillis()+Name;
                 InputStream uploadedStream = item.getInputStream();
                 byte[] buf=new byte[1024];
                 String path=this.getServletContext().getRealPath("/images");
@@ -58,6 +68,7 @@ public class upLoadServlet extends HttpServlet {
                 }
                 fileOutputStream.close();
                 uploadedStream.close();
+                item.delete();
             }
         }
     }
